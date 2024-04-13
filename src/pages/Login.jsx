e3f806b3555b0a985fd327875d85e9b8ec10ad6a
fiveace-Merill <freddie.ogutu@gmail.com>
2024-04-13 17:06:18 +0300

    homepage final update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,11 +7,11 @@ function Login() {
                 className='w-full h-screen flex flex-col align-middle justify-center md:flex-row md:h-5/6 md:w-9/12 md:rounded-3xl md:shadow-xl hover:md:shadow-forest_green-500 hover:md:shadow-2xl'
                 style={{ backgroundImage: `url(${bike})`, backgroundSize: 'cover' }}
             >
-                <div className='mx-5 mb-3 p-2 h-[400px] rounded-2xl bg-forest_green-200 opacity-80 flex flex-col flex-initial md:bg-white md:m-0 md:h-full md:opacity-100 md:items-center'>
+                <div className='mx-5 mb-3 p-2 h-4/6 rounded-2xl bg-forest_green-200 opacity-80 flex flex-col flex-initial md:bg-white md:m-0 md:h-full md:opacity-100 md:items-center'>
                     <div className="flex flex-col mb-auto mt-5 md:w-4/6 md:grow">
                         <h1 className="text-lg font-bold text-center md:text-2xl md:text-wrap md:self-stretch  md:text-forest_green-100">REGISTER FOR AN ACCOUNT</h1>
                         <h3 className="font bold text-md text-center text-forest_green-700 my-2 md:text-[2.5rem]">Welcome!</h3>
-                        <p className='text-lg text-white text-justify m-2 p-2 md:m-0 md:text-2xl md:h-full md:text-forest_green-200'>We just need to get a few details from you to get you signed in for the service.</p>
+                        <p className='text-lg text-white text-justify m-2 p-2 md:m-0 md:text-4xl md:text-center md:mt-10 md:h-full md:text-forest_green-200'>Glad to have you back, just a few steps...</p>
                     </div>
                     <form className="flex flex-col justify-center  self-center w-4/6 h-2/6 md:self-center">
                         <input type='email' placeholder="email" className="my-3 h-8 rounded-lg pl-2 md:border-2 md:border-forest_green-400"></input>
