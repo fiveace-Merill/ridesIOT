82647aa734ff59a3343cb2138676ba354d1efcea
fiveace-Merill <freddie.ogutu@gmail.com>
2024-04-18 16:32:34 +0300

    bike card feature stling final

---
+++ src/components/admin/AddBike.jsx
@@ -0,0 +1,137 @@
+import '../../style/admin/addbike.css'
+
+function AddBike({ bikeDetails, handleSubmit, handleInputChange }) {
+    return (
+        <form onSubmit={handleSubmit} className="md:flex md:flex-col md:h-full text-sm md:grow">
+            <label>
+                Name:
+                <input
+                    type="text"
+                    name="name"
+                    value={bikeDetails.name}
+                    onChange={handleInputChange}
+                    placeholder="Bike Name"
+                />
+            </label>
+            <label>
+                Design:
+                <input
+                    type="text"
+                    name="design"
+                    value={bikeDetails.design}
+                    onChange={handleInputChange}
+                    placeholder="Design"
+                />
+            </label>
+            <label>
+                Price:
+                <input
+                    type="number"
+                    name="price"
+                    value={bikeDetails.price}
+                    onChange={handleInputChange}
+                    placeholder="Price"
+                />
+            </label>
+            <label>
+                <span className='text-md text-center text-robin_egg_blue-400 font-bold ml-5'>Features</span>
+                <fieldset className="md:flex md:flex-col">
+                    <label>
+                        Brake Type:
+                        <input
+                            type="text"
+                            name="brake_type"
+                            value={bikeDetails.features.brake_type}
+                            onChange={handleInputChange}
+                            placeholder="Brakes"
+                        />
+                    </label>
+                    <label>
+                        Wheel Size:
+                        <input
+                            type="number"
+                            name="wheel_size"
+                            value={bikeDetails.features.wheel_size}
+                            onChange={handleInputChange}
+                            placeholder="Wheel Size"
+                        />
+                    </label>
+                    <label>
+                        Frame Material:
+                        <input
+                            type="text"
+                            name="frame_material"
+                            value={bikeDetails.features.frame_material}
+                            onChange={handleInputChange}
+                            placeholder="Frame Material"
+                        />
+                    </label>
+                    <label>
+                        Suspension:
+                        <input
+                            type="text"
+                            name="suspension"
+                            value={bikeDetails.features.suspension}
+                            onChange={handleInputChange}
+                            placeholder="Suspension"
+                        />
+                    </label>
+                    <label>
+                        Weight:
+                        <input
+                            type="number"
+                            name="weight"
+                            value={bikeDetails.features.weight}
+                            onChange={handleInputChange}
+                            placeholder="Weight"
+                        />
+                    </label>
+                    <label>
+                        Gears:
+                        <input
+                            type="number"
+                            name="gears"
+                            value={bikeDetails.features.gears}
+                            onChange={handleInputChange}
+                            placeholder="Gears"
+                        />
+                    </label>
+                    <label>
+                        Color:
+                        <input
+                            type="text"
+                            name="color"
+                            value={bikeDetails.features.color}
+                            onChange={handleInputChange}
+                            placeholder="Color"
+                        />
+                    </label>
+                    <label>
+                        Tires:
+                        <input
+                            type="text"
+                            name="tires"
+                            value={bikeDetails.features.tires}
+                            onChange={handleInputChange}
+                            placeholder="Tires"
+                        />
+                    </label>
+                </fieldset>
+
+            </label>
+            <textarea
+                rows={5}
+                cols={40}
+                name="description"
+                value={bikeDetails.description}
+                onChange={handleInputChange}
+                placeholder="Product Description"
+                className='w-full'
+            >
+            </textarea>
+            <button type="submit" className="bg-robin_egg_blue-400 text-white md:hover:bg-robin_egg_blue-300 md:hover:rounded-full md:hover:text-xs md:w-4/12 h-8 text-sm rounded-md mx-auto my-2">ADD</button>
+        </form>
+    )
+}
+
+export default AddBike
--- src/components/admin/Bike.jsx
+++ src/components/admin/Bike.jsx
@@ -1,22 +1,64 @@
-// import { useState, useEffect } from "react"
+import { useState, useEffect } from "react"
+import AddBike from "./AddBike";
 function Bike() {
-    // const [bike, setBike] = useState()
-    return (
-        <div className="w-full h-full">
-            <div>
-                <h3>Add Bike</h3>
-                <form>
-                    <label>
-                        <input type="text"></input>
+    const [bikeDetails, setBikeDetails] = useState({
+        name: '',
+        design: '',
+        price: '',
+        description: '',
+        features: {
+            brake_type: '',
+            wheel_size: '',
+            frame_material: '',
+            color: '',
+            gears: '',
+            tires: '',
+            weight: '',
+            suspension: '',
+        }
+    });
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        if (name in bikeDetails.features) {
+            setBikeDetails({
+                ...bikeDetails,
+                features: {
+                    ...bikeDetails.features,
+                    [name]: value
+                }
+            });
+        } else {
+            setBikeDetails({
+                ...bikeDetails,
+                [name]: value
+            });
+        }
+    };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Here you would typically send the bikeDetails to a server
+        // or save it in a JSON file as needed
+        console.log(bikeDetails);
+    };
 
-                    </label>
-                </form>
+    useEffect(() => {
+
+    }, [bikeDetails])
+
+    return (
+        <div className="w-full h-[99%] md:bg-slate-100 md:p-3 md:grid md:grid-cols-3 md:gap-3 md:relative">
+            <div className="md:px-5 md:py-2 md:h-[99%] md:rounded-xl md:row-span-2 md:col-span-1 md:row-start-1 md:col-start-1 md:bg-white md:shadow-lg md:shadow-robin_egg_blue-300 md:hover:z-50">
+                <h3 className="text-center text-lg text-robin_egg_blue-400 font-extrabold">Add Bike</h3>
+                <AddBike bikeDetails={bikeDetails} handleSubmit={handleSubmit} handleInputChange={handleInputChange}></AddBike>
             </div>
-            <div>
+            <div className="md:bg-yellow-700 md:rounded-lg">
                 <h3>Delete Bike</h3>
             </div>
-            <div>
+            <div className="md:row-start-2 md:col-2 md:bg-rich_black-500 md:rounded-lg">
                 <h3>Update Bike</h3>
+            </div>
+            <div className="md:col-start-3 md:row-span-2 md:bg-brunswick_green-500 md:rounded-lg">
+                <h3>Get bike</h3>
             </div>
 
         </div>
